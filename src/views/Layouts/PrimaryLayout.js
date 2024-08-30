e6a9a344fd755f654f139fb005f451c9d74e6c15
Cậu Bé Java <154894942+cuongnadev@users.noreply.github.com>
2024-08-31 03:28:06 +0700

    primaryLayout #1

--- src/views/Layouts/PrimaryLayout.js
+++ src/views/Layouts/PrimaryLayout.js
@@ -1,20 +1,33 @@
+import { NavSidebar } from './Components/NavSideBar';
 export class PrimaryLayout {
     // Demo to run login
     constructor() {
         // Global container
         this.globalContainer = document.createElement('div');
-        this.globalContainer.className = 'global-container';
+        this.globalContainer.className = 'global-container flex';
+
+        // initializes content upon successful login
+        window.addEventListener('logging', () => {
+            this.initContent();
+        });
+    }
+    initContent() {
+        // Navigation sidebar
+        this.navSidebar = new NavSidebar();
+        this.globalContainer.append(this.navSidebar.render());
+
+        // Content container
+        this.contentContainer = document.createElement('div');
+        this.contentContainer.className = 'content-container';
+        this.globalContainer.append(this.contentContainer);
     }
 
     render(childNode) {
-        // In ra childNode để kiểm tra
-        console.log('Child Node:', childNode);
-
         // Append childNode to global container
         if (childNode instanceof HTMLElement) {
-            this.globalContainer.appendChild(childNode);
+            this.contentContainer.appendChild(childNode);
         } else if (childNode && typeof childNode.render === 'function') {
-            this.globalContainer.appendChild(childNode.render());
+            this.contentContainer.appendChild(childNode.render());
         }
         return this.globalContainer;
     }
