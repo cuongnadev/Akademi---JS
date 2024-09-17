c9759746370ef3c13b95174b1cb114314d96e615
Cậu Bé Java <154894942+cuongnadev@users.noreply.github.com>
2024-09-17 22:11:10 +0700

    teachers #1

---
+++ src/views/components/TeacherItem/TeacherItem.js
@@ -0,0 +1,68 @@
+import { callIcon, emailIcon, dotsIcon, placeholder } from '~/constants';
+import { Button, buttonSizes, buttonVariants } from '../Button';
+
+export class TeacherItem {
+    constructor(teacher) {
+        this.container = document.createElement('div');
+        this.container.className = 'teacher-item-container flex flex-col items-center';
+
+        // avatar
+        this.avatar = document.createElement('img');
+        this.avatar.className = 'teacher-item-avatar';
+        this.avatar.src = teacher.avatar ? teacher.avatar : placeholder;
+        this.avatar.alt = '';
+
+        // name
+        this.teacherName = document.createElement('h3');
+        this.teacherName.className = 'teacher-item-name';
+        this.teacherName.innerText = teacher.name;
+
+        // major
+        this.major = document.createElement('p');
+        this.major.className = 'teacher-item-major';
+        this.major.innerText = teacher.major;
+
+        // contact
+        // contact
+        // call
+        this.call = new Button(
+            null,
+            callIcon,
+            null,
+            buttonVariants.iconOnly,
+            buttonSizes.iconOnly,
+            'teacher-item-call',
+            () => {},
+        );
+        // mail
+        this.mail = new Button(
+            null,
+            emailIcon,
+            null,
+            buttonVariants.iconOnly,
+            buttonSizes.iconOnly,
+            'teacher-item-mail',
+            () => {},
+        );
+        this.contact = document.createElement('div');
+        this.contact.className = 'teacher-item-contact flex items-center justify-center gap-4';
+        this.contact.append(this.call.render(), this.mail.render());
+
+        // action
+        this.actions = new Button(
+            null,
+            dotsIcon,
+            null,
+            buttonVariants.iconOnly,
+            buttonSizes.iconOnly,
+            'teacher-item-actions',
+            () => {},
+        ).render();
+
+        this.container.append(this.avatar, this.teacherName, this.major, this.contact, this.actions);
+    }
+
+    render() {
+        return this.container;
+    }
+}
--- src/views/pages/Teachers.js
+++ src/views/pages/Teachers.js
@@ -1,1 +1,63 @@
-export class Teachers {}
+import { Pagination, SearchActionsPanel, TeacherItem } from '../components';
+import { TeachersController } from '~/controllers';
+
+export class Teachers {
+    constructor() {
+        this.teachersPerPage = 12;
+        this.currentPage = 1;
+
+        this.container = document.createElement('div');
+        this.container.className = 'teachers-container flex flex-col gap-10';
+
+        // search actions panel
+        this.searchActionsPanel = new SearchActionsPanel('New Teacher');
+
+        // student list
+        this.teachersList = document.createElement('div');
+        this.teachersList.className = 'teachers-list-container';
+
+        // list
+        this.list = document.createElement('div');
+        this.list.className = 'list-teacher-items';
+
+        // pagination
+        this.pagination = new Pagination(this.handlePageChange.bind(this));
+
+        this.teachersList.append(this.list, this.pagination.render());
+
+        this.container.append(this.searchActionsPanel.render(), this.teachersList);
+
+        this.handleListTeachers(TeachersController.getTeachers());
+    }
+
+    async handleListTeachers(data) {
+        this.data = await data;
+        this.updateTeachersList();
+    }
+
+    // Function called on page change
+    handlePageChange(page) {
+        this.currentPage = page;
+        this.updateTeachersList();
+    }
+
+    // Update student list based on current page
+    updateTeachersList() {
+        const start = (this.currentPage - 1) * this.teachersPerPage;
+        const end = start + this.teachersPerPage;
+        const teachersToShow = this.data.slice(start, end);
+
+        this.list.innerHTML = '';
+
+        // Show
+        teachersToShow.forEach((teacher) => {
+            this.teacherItem = new TeacherItem(teacher).render();
+
+            this.list.append(this.teacherItem);
+        });
+    }
+
+    render() {
+        return this.container;
+    }
+}
