8de1295c723cebf66c1c9dfffc823427db63d0b7
Cậu Bé Java <154894942+cuongnadev@users.noreply.github.com>
2024-10-01 22:59:38 +0700

    events #1

--- src/views/components/ScheduleDetails/ScheduleDetails.js
+++ src/views/components/ScheduleDetails/ScheduleDetails.js
@@ -1,11 +1,12 @@
 import { createContainer, formatDate } from '~/utils';
 import { ScheduleItem } from './ScheduleItem';
 import { StudentsController, TeachersController } from '~/controllers';
+import { Button, buttonSizes, buttonVariants } from '../Button';
 
 export class ScheduleDetails {
     constructor(role, id) {
         this.container = document.createElement('div');
-        this.container.className = 'schedule-details';
+        this.container.className = 'schedule-details flex flex-col';
 
         // schedule list
         this.scheduleList = document.createElement('div');
@@ -26,32 +27,55 @@ export class ScheduleDetails {
 
         this.headerSchedule.append(this.headerScheduleTitle, this.headerScheduleSubTitle);
 
-        this.scheduleList.append(this.headerSchedule);
-
         this.createScheduleItem(role, id);
 
         this.container.appendChild(this.scheduleList);
+    }
+
+    renderScheduleItem(role, data) {
+        this.scheduleList.innerHTML = '';
+        this.scheduleList.append(this.headerSchedule);
+
+        data.map((course) => {
+            this.scheduleItem = new ScheduleItem(
+                role,
+                course.name,
+                `Class ${course.class}`,
+                course.major,
+                course.dow,
+                course.time,
+            );
+            this.scheduleList.append(this.scheduleItem.render());
+        });
     }
 
     async createScheduleItem(role, id) {
         try {
             const data =
                 role === 'Teacher'
-                    ? await TeachersController.getTeacherSchedule(id.teacherId)
+                    ? id
+                        ? await TeachersController.getTeacherSchedule(id.teacherId)
+                        : await TeachersController.getAllTeacherSchedule()
                     : await StudentsController.getStudentSchedule(id.studentId);
 
             if (Array.isArray(data) && data.length > 0) {
-                data.map((course) => {
-                    this.scheduleItem = new ScheduleItem(
-                        role,
-                        course.name,
-                        `Class ${course.class}`,
-                        course.major,
-                        course.dow,
-                        course.time,
+                if (data.length > 4) {
+                    const sliceData = data.slice(0, 4);
+                    this.renderScheduleItem(role, sliceData);
+
+                    this.buttonViewMore = new Button(
+                        'View More',
+                        null,
+                        null,
+                        buttonVariants.filled,
+                        buttonSizes.md,
+                        'schedule-details-view-more',
+                        () => {},
                     );
-                    this.scheduleList.append(this.scheduleItem.render());
-                });
+                    this.container.append(this.buttonViewMore.render());
+                } else {
+                    this.renderScheduleItem(role, data);
+                }
             } else {
                 const noScheduleMessage = document.createElement('p');
                 noScheduleMessage.className = 'schedule-details-not-schedule';
@@ -62,6 +86,7 @@ export class ScheduleDetails {
             const noScheduleMessage = document.createElement('p');
             noScheduleMessage.className = 'schedule-details-not-schedule';
             noScheduleMessage.innerText = 'No Schedule';
+            console.log(error);
             this.scheduleList.append(noScheduleMessage);
         }
     }
--- src/views/pages/Events.js
+++ src/views/pages/Events.js
@@ -1,1 +1,70 @@
-export class Events {}
+import { dropdownIcon } from '~/constants';
+import { Button, buttonSizes, buttonVariants, ScheduleDetails } from '../components';
+import { createContainer } from '~/utils';
+
+const monthNames = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+];
+export class Events {
+    constructor() {
+        this.container = document.createElement('div');
+        this.container.className = 'events-container flex gap-10';
+
+        // calendar
+        this.calendar = document.createElement('div');
+        this.calendar.className = 'events-calendar flex flex-col gap-10 flex-1';
+        // header
+        this.header = document.createElement('div');
+        this.header.className = 'events-header flex items-center justify-between';
+        // title
+        this.title = document.createElement('h3');
+        this.title.className = 'events-title';
+        this.title.innerHTML = 'Calendar';
+        // actions
+        this.mouth = new Button(
+            `${monthNames[new Date().getMonth()]}`,
+            null,
+            dropdownIcon,
+            buttonVariants.outlined,
+            buttonSizes.md,
+            'events-action-item btn-mouth',
+            () => {},
+        );
+        this.year = new Button(
+            `${new Date().getFullYear()}`,
+            null,
+            dropdownIcon,
+            buttonVariants.outlined,
+            buttonSizes.md,
+            'events-action-item btn-year',
+            () => {},
+        );
+        this.actions = createContainer(
+            'events-actions flex items-center gap-6',
+            this.mouth.render(),
+            this.year.render(),
+        );
+
+        this.header.append(this.title, this.actions);
+        this.calendar.append(this.header);
+        // schedule
+        this.schedule = new ScheduleDetails('Teacher');
+
+        this.container.append(this.calendar, this.schedule.render());
+    }
+
+    render() {
+        return this.container;
+    }
+}
